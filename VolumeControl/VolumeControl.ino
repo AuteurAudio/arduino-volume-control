35564b1ed7cda3b430c89b4fae81f0c169b40e3d
Alexander V <>
2021-02-28 21:47:35 +0200

    removed channel & relay code

--- VolumeControl/VolumeControl.ino
+++ VolumeControl/VolumeControl.ino
@@ -19,42 +19,12 @@
 #define volumeClockPin         8         // OUTPUT Clock pin for volume control
 #define volumeDataPin          11         // OUTPUT Data pin for volume control
 #define volumeSelectPin        10        // OUTPUT Select pin for volume control
-#define volumeMutePin          9        // OUTPUT Mute pin for the volume control  
+#define volumeMutePin          9        // OUTPUT Mute pin for the volume control
 
-// Relating to debouncing the mute and channel select pins
-#define muteSwitchPin          5         // INPUT Momentary switch for mute
-#define channelSwitchPin       6         // Toggle input for channel select
-#define outputSwitchPin        4         // Toggle for impedance control for output
-
-// Output pin to control input channel relay and output impedance shunt relay
-#define channelRelayPin        12        // Pin to control the relay for channel selection
-#define outputRelayPin         7         // Pin to control relay for output for turnout delay
-
-// XOR logic reversal for switches and relays
-// Somewhat redundant, but allows the logic states to be deterministic on both input and output
-#define channelSwitchReverse   HIGH
-#define channelRelayReverse    HIGH
-#define outputSwitchReverse    HIGH
-#define outputRelayReverse     HIGH
-#define balanceSwitchReverse   LOW
-
-// Maximum (byte value) of the volume to send to the PGA2311
+// Maximum (byte value) of the volume to send to the PGA4311
 // This is here to avoid regions where the high gains has too high S/N
 // (192 is 0 dB -- e.g. no gain)
 #define maximumVolume          192
-
-
-
-/*
- * EEPROM usage map (use imagination if XOR logic is changed
- *
- * 0: Balance setpoint if compiled 
- * 3: Volume for channel one / output one
- * 4: Volume for channel one / output two
- * 5: Volume for channel two / output one
- * 6: Volume for channel two / output two
- */
- 
 
 
 /*
@@ -78,13 +48,10 @@ long int  delayTimeout;
 int       isMuted =          false;            // Internally used status for mute state
 int       channelRun =       false;            // Internally used status for passing control to channel 
 int       outputRun =        false;            // Internally used status for passing control to output 
-long int  selectedChannel;
-long int  selectedOutput;
-
 
 
 /*
- * Inline function to write a byte to the PGA2311
+ * Inline function to write a byte to the PGA4311
  */
 
 static inline void byteWrite(byte byteOut){
@@ -97,8 +64,6 @@ static inline void byteWrite(byte byteOut){
      byteOut<<=1;   
    }
 }
- 
- 
  
 /*
  * Function to set the (stereo) volume on the PGA4311
@@ -181,11 +146,11 @@ void setup(){
   Serial.begin(38400);
   Serial.println("Initializing...");
   
-  // Mute the PGA2311
+  // Mute the PGA4311
   pinMode(volumeMutePin,OUTPUT);
   digitalWrite(volumeMutePin,LOW);           
   
-  // Set up control pins for PGA2311
+  // Set up control pins for PGA4311
   pinMode(volumeSelectPin,OUTPUT);
   pinMode(volumeClockPin,OUTPUT);
   pinMode(volumeDataPin,OUTPUT);
@@ -193,54 +158,13 @@ void setup(){
   digitalWrite(volumeClockPin,HIGH);
   digitalWrite(volumeDataPin,HIGH);
   
-  // Initialize other I/O pins
-  pinMode(muteSwitchPin,INPUT);
-  pinMode(channelSwitchPin,INPUT);
-  pinMode(outputSwitchPin,INPUT);
-  pinMode(channelRelayPin,OUTPUT);
-  pinMode(outputRelayPin,OUTPUT);
-  
   // Delay a bit to wait for the state of the switches
-  delay(100);  
-  
-  // Read the channel switch and set the appropriate volume
-  channelSwitch.update();
-  if(channelSwitch.read()==(HIGH^channelSwitchReverse)){
-    // Channel One   
-    selectedChannel=1;
-    Serial.println("Using channel one...");
-    digitalWrite(channelRelayPin,HIGH^channelRelayReverse);
-  }
-  else{
-    // Channel Two
-    selectedChannel=2;
-    Serial.println("Using channel two...");
-    digitalWrite(channelRelayPin,LOW^channelRelayReverse);
-  }  
+  delay(100);
   
   // Set volume to 0 for later soft volume start
   setVolume(0);
-  
-  // Read the output relay switch and set it appropriately
-  outputSwitch.update();
-  if(outputSwitch.read()==(HIGH^outputSwitchReverse)){
-    selectedOutput=1;
-    Serial.println("Using output channel one...");
-    digitalWrite(outputRelayPin,HIGH^outputRelayReverse);
-    channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-  }
-  else{
-    selectedOutput=2;
-    Serial.println("Using output channel two...");
-    digitalWrite(outputRelayPin,LOW^outputRelayReverse);
-    channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-    
-    // Safegaurd for the maximum
-    if(channelVolume>maximumVolume){
-      channelVolume=maximumVolume;
-    }
-    
-  }  
+
+  channelVolume=EEPROM.read(0);
   
   tempChannelVolume=channelVolume;
   
@@ -250,7 +174,7 @@ void setup(){
   // Wait a bit for the whole system to come online
   delay(800);
   
-  // Unmute the PGA2311
+  // Unmute the PGA4311
   digitalWrite(volumeMutePin,HIGH); 
 
   // Wait a bit
@@ -263,260 +187,12 @@ void setup(){
   volumeKnob.write(0);
   
 }
-
- 
- 
  
 /*
  * Main Arduino loop 
  */
  
-void loop(){     
-  
-    /*
-     * OUTPUT SWITCH
-     */
-     
-     // Only need to do this on switch state change
-     if(outputSwitch.update() || outputRun){
-       
-       // Address the change
-       if(outputSwitch.read()==(HIGH^outputSwitchReverse)){
-         
-         // Channel one
-         if(selectedOutput==2){
-            Serial.println("Switching to output one...");
-            selectedOutput=1;
-       
-            // Scale the volume down if it is not muted
-            if(isMuted==false && outputRun==false){
-              scaleVolume(channelVolume,0,35);
-       
-              // Mute the PGA2311
-              digitalWrite(volumeMutePin,LOW); 
-            }
-            // Reset the control variable
-            outputRun=false;
-            
-            // Load the volume value
-            channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-            
-            // Safegaurd for the maximum
-            if(channelVolume>maximumVolume){
-              channelVolume=maximumVolume;
-            }
-            tempChannelVolume=channelVolume;
-            
-            // Switch the channel relay
-            digitalWrite(outputRelayPin,HIGH^outputRelayReverse);
-            
-            // Delay a bit for the relay to switch
-            delay(200);
-            
-            if(isMuted==false){
-              // Unmute the PGA2311
-              digitalWrite(volumeMutePin,HIGH); 
-              
-              // Delay 
-              delay(20);
-              
-              // Check the status of the channel switch 
-              if(channelSwitch.update()){
-                // Just set the flag to run through the channel logic
-                channelRun=true;
-              }
-              else{
-                // Otherwise scale the volume back up
-                scaleVolume(0,channelVolume,50);
-              }
-            }
-         }
-       }
-
-       // Channel two
-       else{
-         if(selectedOutput==1){
-            Serial.println("Switching to output two...");
-            selectedOutput=2;
-        
-            // Scale the volume down if it is not muted
-            if(isMuted==false && outputRun==false){
-              scaleVolume(channelVolume,0,35);
-              
-              // Mute the PGA2311
-              digitalWrite(volumeMutePin,LOW); 
-            }  
-            // Reset the control variable
-            outputRun=false;
-              
-            // Load the volume value
-            channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-            
-            // Safegaurd for the maximum
-            if(channelVolume>maximumVolume){
-              channelVolume=maximumVolume;
-            }
-            tempChannelVolume=channelVolume;
-            
-            // Switch the channel relay
-            digitalWrite(outputRelayPin,LOW^outputRelayReverse);
-            
-            // delay a bit for the relay to switch
-            delay(200);
-            
-            if(isMuted==false){
-              // Unmute the PGA2311
-              digitalWrite(volumeMutePin,HIGH); 
-              
-              // Delay 
-              delay(20);
-              
-              // Check the status of the channel switch 
-              if(channelSwitch.update()){
-                // Just set the flag to run through the channel logic
-                channelRun=true;
-              }
-              else{
-                // Otherwise scale the volume back up
-                scaleVolume(0,channelVolume,50);
-              }
-           }
-         }
-      }  
-   }
-    
-     
-    /*
-     * CHANNEL SELECT
-     */
-     
-     // Only need to do this on switch state change
-     if(channelSwitch.update() || channelRun){
-       
-       // Address the change
-       if(channelSwitch.read()==(HIGH^channelSwitchReverse)){
-         // Channel one
-         if(selectedChannel==2){
-            Serial.println("Switching to channel one...");
-            selectedChannel=1;
-       
-            // Scale the volume down if it is not muted
-            if(isMuted==false && channelRun==false){
-              scaleVolume(channelVolume,0,35);
-       
-              // Mute the PGA2311
-              digitalWrite(volumeMutePin,LOW); 
-            }
-            // Reset channelRun
-            channelRun=false;
-            
-            // Load the volume value
-            channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-            
-            // Safegaurd for the maximum
-            if(channelVolume>maximumVolume){
-              channelVolume=maximumVolume;
-            }
-            tempChannelVolume=channelVolume;
-            
-            // Switch the channel relay
-            digitalWrite(channelRelayPin,HIGH^channelRelayReverse);
-            
-            // Delay a bit for the relay to switch
-            delay(200);
-            
-            if(isMuted==false){
-              // Unmute the PGA2311
-              digitalWrite(volumeMutePin,HIGH); 
-              
-              // Delay 
-              delay(20);
-              
-              // Check the status of the output switch 
-              if(outputSwitch.update()){
-                // Just set the flag to run through the channel logic
-                outputRun=true;
-              }
-              else{
-                // Otherwise scale the volume back up
-                scaleVolume(0,channelVolume,50);
-              }
-            }
-         }
-       }
-    
-       // Channel two
-       else{
-         if(selectedChannel==1){
-            Serial.println("Switching to channel two...");
-            selectedChannel=2;
-        
-            // Scale the volume down if it is not muted
-            if(isMuted==false && channelRun==false){
-              scaleVolume(channelVolume,0,35);
-              
-              // Mute the PGA2311
-              digitalWrite(volumeMutePin,LOW); 
-            }  
-            // Reset channelRun
-            channelRun=false;
-              
-            // Load the volume value
-            channelVolume=EEPROM.read(2*selectedOutput+selectedChannel);
-            
-            // Safegaurd for the maximum
-            if(channelVolume>maximumVolume){
-              channelVolume=maximumVolume;
-            }
-            tempChannelVolume=channelVolume;
-            
-            // Switch the channel relay
-            digitalWrite(channelRelayPin,LOW^channelRelayReverse);
-            
-            // delay a bit for the relay to switch
-            delay(200);
-            
-            if(isMuted==false){
-              // Unmute the PGA2311
-              digitalWrite(volumeMutePin,HIGH); 
-              
-              // Delay 
-              delay(20);
-              
-              // Check the status of the output switch 
-              if(outputSwitch.update()){
-                // Just set the flag to run through the channel logic
-                outputRun=true;
-              }
-              else{
-                // Otherwise scale the volume back up
-                scaleVolume(0,channelVolume,50);
-              }
-            }
-         }
-      }  
-   }
-    
-    
-  /*
-   * MUTE BUTTON
-   */
-  
-  if(muteSwitch.update()){
-     if(muteSwitch.read()==HIGH){
-       if(isMuted==false){
-         isMuted=true;
-         Serial.println("Muting...");
-         scaleVolume(channelVolume,0,35);
-       }
-       else{
-         isMuted=false;
-         Serial.println("Unmuting...");
-         scaleVolume(0,channelVolume,50);
-       }
-     }
-   }
-  
+void loop(){
   
  /*
   * VOLUME 
@@ -552,7 +228,7 @@ void loop(){
         }
         else{
           // Save the volume setting
-          EEPROM.write(2*selectedOutput+selectedChannel,channelVolume);
+          EEPROM.write(0,channelVolume);
           
           // Print it out for debug
           Serial.print(31.5-((255-(float)channelVolume)/2));
